6eefc50738965e5735ee89390f5662f2e4575731
bkobei <brandon3igit@gmail.com>
2024-09-26 21:37:36 -0700

    Updated Utilty bot controller

--- Assets/Scripts/PlayerControllers/PlayerUtilityController.cs
+++ Assets/Scripts/PlayerControllers/PlayerUtilityController.cs
@@ -8,6 +8,18 @@ public class PlayerUtilityController : MonoBehaviour
     [SerializeField] private float speed = 5f;
     //[SerializeField] private float speedRotate = 100f;
 
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+
+    public float groundCheckRadius = 0.0f;
+
+    // public float groundCheckDistance;
+
+    [SerializeField] private bool isGrounded = false;
+    private bool canJump = false;
+
+    private Vector3 direction;
+
     // Update is called once per frame
     void Start()
     {
@@ -16,35 +28,76 @@ public class PlayerUtilityController : MonoBehaviour
 
     void Update()
     {
-        /* This was a temporary movement for my cube, will add stuff to the controller later.
-        if (Input.GetKey("i"))
+        // This was a temporary movement for my cube, will add stuff to the controller later.
+        CheckIfCanJump();
+
+        if (Input.GetKey(KeyCode.I))
         {
             Vector3 movement = new Vector3(0f, 0f, 1f * Time.deltaTime * speed);
             movement = transform.TransformDirection(movement);
             transform.position += movement;
         }
-        if (Input.GetKey("j"))
+        if (Input.GetKey(KeyCode.J))
         {
             Vector3 movement = new Vector3(-1f * Time.deltaTime * speed, 0, 0);
             movement = transform.TransformDirection(movement);
             transform.position += movement;
         }
-        if (Input.GetKey("k"))
+        if (Input.GetKey(KeyCode.K))
         {
             Vector3 movement = new Vector3(0f, 0f, -1f * Time.deltaTime * speed);
             movement = transform.TransformDirection(movement);
             transform.position += movement;
         }
-        if (Input.GetKey("l"))
+        if (Input.GetKey(KeyCode.L))
         {
             Vector3 movement = new Vector3(1f * Time.deltaTime * speed, 0, 0);
             movement = transform.TransformDirection(movement);
             transform.position += movement;
         }
-        if (Input.GetKey("space"))
+        if (Input.GetKey(KeyCode.Space) && canJump)
         {
-            Debug.Log("Jumped");
+            Vector3 jump = new Vector3(0f, 1f, 0f);
+            rb.AddForce(jump * 0.2f, ForceMode.Impulse);
         }
-        */
+        if (Input.GetKey("o"))
+        {
+            Debug.Log("Shoot Stun Projectile");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckSurrounding();
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void CheckSurrounding()
+    {
+        if (CheckIfGrounded())
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private bool CheckIfGrounded()
+    {
+        RaycastHit hit;
+        return Physics.Raycast(groundCheck.position, -transform.up, out hit, groundCheckRadius);
     }
 }
